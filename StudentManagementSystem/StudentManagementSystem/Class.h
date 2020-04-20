e11bdefebee9aece1382cb56b056c3ea71733e31
VuongPhuc <phuc16102001@gmail.com>
2020-04-20 22:38:27 +0700

    .

---
+++ StudentManagementSystem/StudentManagementSystem/Class.h
@@ -0,0 +1,14 @@
+#ifndef _CLASS_HEADER_
+#define _CLASS_HEADER_
+
+#include "Constant.h"
+#include "Account.h"
+
+struct Class {
+	string className;
+	int nStudent;
+	string* studentID;
+};
+
+
+#endif
