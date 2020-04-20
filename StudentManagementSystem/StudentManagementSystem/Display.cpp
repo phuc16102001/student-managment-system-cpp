0f327cf54cc63671a79c28b92c7fb889f003ae70
buingocchinh321 <63283198+buingocchinh321@users.noreply.github.com>
2020-04-20 15:56:26 +0700

    add 3 function display(staff,lec,stu)

--- StudentManagementSystem/StudentManagementSystem/Display.cpp
+++ StudentManagementSystem/StudentManagementSystem/Display.cpp
@@ -15,3 +15,62 @@ void displayLogin(string& inputAccountID,string& inputPassword) {
 	cout << "Password: ";
 	getline(cin, inputPassword);
 }
+
+int displayStaffMenu()
+{
+	int x;
+	cout << "Hello\nWhat do you want to do ?\n ";
+	cout << "Course:" << endl;
+	cout << "1.Looking for a student ?" << endl
+		<< "2.Add a student ?" << endl
+		<< "3.Edit information of a student ?" << endl
+		<< "4.Remove a student ?" << endl
+		<< "5.Change students to another class ?" << endl
+		<< "6.View list of classes ?" << endl
+		<< "7.View list of students in a class ?" << endl
+		<< "8.Create/ Update/ Delete/ View academic year, and semester ?" << endl
+		<< "9.Import courses from a semester ?" << endl
+		<< "10.Manually add a new course ?" << endl
+		<< "11.Edit an existing course ? " << endl
+		<< "12.Remove course ?" << endl
+		<< "13.Remove a specific student from a course ? " << endl
+		<< "14.Add a specific student to a course ? " << endl
+		<< "15.View list of course in the current semester ?" << endl
+		<< "16.View list of students of a course ? " << endl
+		<< "17.View attendance list of a course ?" << endl
+		<< "18.Create/ Update/ Delete/ View all lecturers ?"
+		<< endl << endl;
+	cout << "Scoreboard: \n";
+	cout << "19.Search and view attendance list of a course" << endl
+		<< "20.Export a scoreboard into a csv file" << endl << endl;
+	cout << "Please, choose one function \n";
+	cin >> x;
+	return x;
+}
+int displayLectureMenu()
+{
+	int x;
+	cout << "Hello\nWhat do you want to do ?\n ";
+	cout << "1.View list of courses in the current semester" << endl
+		<< "2.View list of students of a course" << endl
+		<< "3.View attendance list of a course" << endl
+		<< "4.Edit attendance" << endl
+		<< "5.Import scoreboard of a course" << endl
+		<< "6.Edit grade of a student" << endl
+		<< "7.View a scoreboard" << endl << endl;
+	cout << "Please, choose one function \n";
+	cin >> x;
+	return x;
+}
+int displayStudentMenu()
+{
+	int x;
+	cout << "Hello\nWhat do you want to do ?\n ";
+	cout << "1.Check-in" << endl
+		<< "2.View check-in result" << endl
+		<< "3.View schedules" << endl
+		<< "4.View your scores of a course" << endl << endl;
+		cout << "Please, choose one function \n";
+	cin >> x;
+	return x;
+}
