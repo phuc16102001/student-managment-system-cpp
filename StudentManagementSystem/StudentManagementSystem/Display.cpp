afeba590f351f733e0455bb29ddb7f6516e08cc4
VuongPhuc <phuc16102001@gmail.com>
2020-04-21 14:37:55 +0700

    Comment and rewrite some display function, write the display function in main source base on account type

--- StudentManagementSystem/StudentManagementSystem/Display.cpp
+++ StudentManagementSystem/StudentManagementSystem/Display.cpp
@@ -18,43 +18,46 @@ void displayLogin(string& inputAccountID,string& inputPassword) {
 
 int displayStaffMenu()
 {
+	displayHeaderUI();
 	int x;
-	cout << "Hello\nWhat do you want to do ?\n ";
+	cout << "What do you want to do ?\n";
 	cout << "Course:" << endl;
-	cout << "1.Looking for a student ?" << endl
-		<< "2.Add a student ?" << endl
-		<< "3.Edit information of a student ?" << endl
-		<< "4.Remove a student ?" << endl
-		<< "5.Change students to another class ?" << endl
-		<< "6.View list of classes ?" << endl
-		<< "7.View list of students in a class ?" << endl
-		<< "8.Create/ Update/ Delete/ View academic year, and semester ?" << endl
-		<< "9.Import courses from a semester ?" << endl
-		<< "10.Manually add a new course ?" << endl
-		<< "11.Edit an existing course ? " << endl
-		<< "12.Remove course ?" << endl
-		<< "13.Remove a specific student from a course ? " << endl
-		<< "14.Add a specific student to a course ? " << endl
-		<< "15.View list of course in the current semester ?" << endl
-		<< "16.View list of students of a course ? " << endl
-		<< "17.View attendance list of a course ?" << endl
-		<< "18.Create/ Update/ Delete/ View all lecturers ?"
+	cout << "1.Looking for a student" << endl
+		<< "2.Add a student" << endl
+		<< "3.Edit information of a student" << endl
+		<< "4.Remove a student" << endl
+		<< "5.Change students to another class" << endl
+		<< "6.View list of classes" << endl
+		<< "7.View list of students in a class" << endl
+		<< "8.Create/ Update/ Delete/ View academic year, and semester" << endl
+		<< "9.Import courses from a semester" << endl
+		<< "10.Manually add a new course" << endl
+		<< "11.Edit an existing course " << endl
+		<< "12.Remove course" << endl
+		<< "13.Remove a specific student from a course" << endl
+		<< "14.Add a specific student to a course" << endl
+		<< "15.View list of course in the current semester" << endl
+		<< "16.View list of students of a course " << endl
+		<< "17.View attendance list of a course" << endl
+		<< "18.Create/ Update/ Delete/ View all lecturers"
 		<< endl << endl;
 	cout << "Scoreboard: \n";
 	cout << "19.Search and view attendance list of a course" << endl
 		<< "20.Export a scoreboard into a csv file" << endl << endl;
-	cout << "Please, choose one function \n";
+	cout << "Choose one function: ";
 	cin >> x;
-	while (x < 0 || x > 20)
-	{
-		cout << "Pls, re-enter your choice\n"; cin >> x;
+	while (x < 0 || x > 20) {
+		cout << "Please, re-enter your choice\n"; 
+		cin >> x;
 	}
 	return x;
 }
+
 int displayLectureMenu()
 {
+	displayHeaderUI();
 	int x;
-	cout << "Hello\nWhat do you want to do ?\n ";
+	cout << "What do you want to do ?\n";
 	cout << "1.View list of courses in the current semester" << endl
 		<< "2.View list of students of a course" << endl
 		<< "3.View attendance list of a course" << endl
@@ -62,27 +65,29 @@ int displayLectureMenu()
 		<< "5.Import scoreboard of a course" << endl
 		<< "6.Edit grade of a student" << endl
 		<< "7.View a scoreboard" << endl << endl;
-	cout << "Please, choose one function \n";
+	cout << "Choose one function: ";
 	cin >> x;
-	while (x < 0 || x > 7)
-	{
-		cout << "Pls, re-enter your choice\n"; cin >> x;
+	while (x < 0 || x > 7) {
+		cout << "Please, re-enter your choice"; 
+		cin >> x;
 	}
 	return x;
 }
+
 int displayStudentMenu()
 {
+	displayHeaderUI();
 	int x;
-	cout << "Hello\nWhat do you want to do ?\n ";
+	cout << "What do you want to do ?\n";
 	cout << "1.Check-in" << endl
 		<< "2.View check-in result" << endl
 		<< "3.View schedules" << endl
 		<< "4.View your scores of a course" << endl << endl;
-		cout << "Please, choose one function \n";
+	cout << "Choose one function: ";
 	cin >> x;
-	while (x < 0 || x > 4)
-	{
-		cout << "Pls, re-enter your choice\n"; cin >> x;
+	while (x < 0 || x > 4) {
+		cout << "Please, re-enter your choice\n"; 
+		cin >> x;
 	}
 	return x;
 }
--- StudentManagementSystem/StudentManagementSystem/MainSource.cpp
+++ StudentManagementSystem/StudentManagementSystem/MainSource.cpp
@@ -4,15 +4,43 @@
 
 void login(AccountList* accountList, Account& accountLogin) {
 	while (true) {
+		//Get input
 		string inputAccountID, inputPassword;
 		displayLogin(inputAccountID, inputPassword);
+		
+		//Verify login
 		if (findAccountID(inputAccountID, accountList, accountLogin)) {
-			if (checkPassword(inputPassword, accountLogin)) return;
+			if (checkPassword(inputPassword, accountLogin)) break;
 		}
+
+		//Incorrect account
 		cout << "Incorrect information\n";
 		cout << "Press enter to try again...\n";
 		cin.get();
 		system("CLS");
+	}
+
+	system("CLS");
+}
+
+void menu(Account accountLogin) {
+	//Display function menu
+	switch (accountLogin.accountType) {
+		case (0): {
+			//Display staff menu
+			displayStaffMenu();
+			break;
+		}
+		case (1): {
+			//Display lecture menu
+			displayLectureMenu();
+			break;
+		}
+		case (2): {
+			//Display student menu
+			displayStudentMenu();
+			break;
+		}
 	}
 }
 
@@ -22,8 +50,11 @@ void demo() {
 	Account accountLogin;
 	importAccountFromStorage(accountList);
 
+	//Display Login
 	login(accountList, accountLogin);
+	menu(accountLogin);
 
+	//Clear account list
 	clearAccountList(accountList);
 }
 
