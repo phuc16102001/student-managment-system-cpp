e79b288ce721f3091a691e489f0886633766a9c0
Thanh Hoang <sinsin.4869@gmail.com>
2020-04-21 19:52:25 +0700

    Update Display.cpp

--- StudentManagementSystem/StudentManagementSystem/Display.cpp
+++ StudentManagementSystem/StudentManagementSystem/Display.cpp
@@ -20,34 +20,34 @@ int displayStaffMenu()
 {
 	displayHeaderUI();
 	int x;
-	cout << "What do you want to do ?\n";
+	cout << "Staff Menu?\n";
 	cout << "Course:" << endl;
-	cout << "1.Looking for a student" << endl
-		<< "2.Add a student" << endl
-		<< "3.Edit information of a student" << endl
-		<< "4.Remove a student" << endl
-		<< "5.Change students to another class" << endl
-		<< "6.View list of classes" << endl
-		<< "7.View list of students in a class" << endl
-		<< "8.Create/ Update/ Delete/ View academic year, and semester" << endl
-		<< "9.Import courses from a semester" << endl
-		<< "10.Manually add a new course" << endl
-		<< "11.Edit an existing course " << endl
-		<< "12.Remove course" << endl
-		<< "13.Remove a specific student from a course" << endl
-		<< "14.Add a specific student to a course" << endl
-		<< "15.View list of course in the current semester" << endl
-		<< "16.View list of students of a course " << endl
-		<< "17.View attendance list of a course" << endl
-		<< "18.Create/ Update/ Delete/ View all lecturers"
+	cout << "1. Find student" << endl
+		<< "2. Add student to a class" << endl
+		<< "3. Edit student information" << endl
+		<< "4. Remove student from a class" << endl
+		<< "5. Move student to another class" << endl
+		<< "6. View list of classes" << endl
+		<< "7. View list of students in a class" << endl
+		<< "8. Create/Update/Delete/View academic year and semester" << endl
+		<< "9. Import courses from a semester" << endl
+		<< "10. Add new course" << endl
+		<< "11. Edit existing course" << endl
+		<< "12. Remove course" << endl
+		<< "13. Remove student from a course" << endl
+		<< "14. Add student to a course" << endl
+		<< "15. View list of course in the current semester" << endl
+		<< "16. View list of students of a course " << endl
+		<< "17. View attendance list of a course" << endl
+		<< "18. Create/Update/Delete/View all lecturers"
 		<< endl << endl;
 	cout << "Scoreboard: \n";
-	cout << "19.Search and view attendance list of a course" << endl
-		<< "20.Export a scoreboard into a csv file" << endl << endl;
+	cout << "19. Search and view attendance list of a course" << endl
+		<< "20. Export scoreboard into csv file" << endl << endl;
 	cout << "Choose one function: ";
 	cin >> x;
 	while (x < 0 || x > 20) {
-		cout << "Please, re-enter your choice\n"; 
+		cout << "Please choose again!\n"; 
 		cin >> x;
 	}
 	return x;
@@ -57,18 +57,18 @@ int displayLectureMenu()
 {
 	displayHeaderUI();
 	int x;
-	cout << "What do you want to do ?\n";
-	cout << "1.View list of courses in the current semester" << endl
-		<< "2.View list of students of a course" << endl
-		<< "3.View attendance list of a course" << endl
-		<< "4.Edit attendance" << endl
-		<< "5.Import scoreboard of a course" << endl
-		<< "6.Edit grade of a student" << endl
-		<< "7.View a scoreboard" << endl << endl;
+	cout << "Lecturer Menu\n";
+	cout << "1. View list of courses in the current semester" << endl
+		<< "2. View list of students of a course" << endl
+		<< "3. View attendance list of a course" << endl
+		<< "4. Edit attendances" << endl
+		<< "5. Import scoreboard" << endl
+		<< "6. Edit student grade" << endl
+		<< "7. Scoreboard" << endl << endl;
 	cout << "Choose one function: ";
 	cin >> x;
 	while (x < 0 || x > 7) {
-		cout << "Please, re-enter your choice"; 
+		cout << "Please choose again!"; 
 		cin >> x;
 	}
 	return x;
@@ -78,15 +78,15 @@ int displayStudentMenu()
 {
 	displayHeaderUI();
 	int x;
-	cout << "What do you want to do ?\n";
-	cout << "1.Check-in" << endl
-		<< "2.View check-in result" << endl
-		<< "3.View schedules" << endl
-		<< "4.View your scores of a course" << endl << endl;
+	cout << "Student Menu\n";
+	cout << "1. Check-in" << endl
+		<< "2. View check-in result" << endl
+		<< "3. View schedules" << endl
+		<< "4. View your scores" << endl << endl;
 	cout << "Choose one function: ";
 	cin >> x;
 	while (x < 0 || x > 4) {
-		cout << "Please, re-enter your choice\n"; 
+		cout << "Please choose again!\n"; 
 		cin >> x;
 	}
 	return x;
