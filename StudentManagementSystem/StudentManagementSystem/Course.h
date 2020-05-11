48f44f312b534373a836ba92042031825b3bc019
Thanh Hoang <64027510+thanhhoang4869@users.noreply.github.com>
2020-05-11 23:30:28 +0700

    Create course + score struct

--- StudentManagementSystem/StudentManagementSystem/Course.h
+++ StudentManagementSystem/StudentManagementSystem/Course.h
@@ -3,4 +3,30 @@
 
 #include "Constant.h"
 
+struct Course {
+	string courseID;
+	string courseName;
+	string className;
+	string lecturerAccount;
+	string startDate;
+	string endDate;
+	int dayOfWeek;
+	int startHour, startMinute;
+	int endHour, endMinute;
+};
+
+struct CourseList {
+	Course* courseData;
+	CourseList* nextCourse;
+};
+
+struct Score {
+	float midScore, bonusScore, finalScore, totalScore;
+};
+
+struct ScoreList {
+	Score* scoreData;
+	ScoreList* nextScore;
+};
+
 #endif
