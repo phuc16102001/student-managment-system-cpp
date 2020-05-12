bacbf0ccc3a2ed4509865ae5c7ef82c39a9ca5ae
VuongPhuc <phuc16102001@gmail.com>
2020-05-12 07:30:15 +0700

    clearCourseList

--- StudentManagementSystem/StudentManagementSystem/Course.cpp
+++ StudentManagementSystem/StudentManagementSystem/Course.cpp
@@ -9,5 +9,17 @@ bool saveCourseToStorage(string semester, CourseList* courseList) {
 }
 
 void clearCourseList(CourseList*& courseList) {
+	while (courseList != nullptr) {
+		CourseList* tempCourse = courseList;
+		courseList = courseList->nextCourse;
 
+		ScoreList* scoreList = tempCourse->courseData->scoreList;
+		while (scoreList != nullptr) {
+			ScoreList* tempScore = scoreList;
+			scoreList = scoreList->nextScore;
+			delete tempScore;
+		}
+
+		delete tempCourse;
+	}
 }
